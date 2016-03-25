a3592c23353f5a863fd04acaf6ecb55cf1c53cea
scamscam <chubenkoroman@yandex.ru>
2016-03-25 17:04:32 +0300

    Update Potential.java

--- CallCentre/src/com/callcenter/Potential.java
+++ CallCentre/src/com/callcenter/Potential.java
@@ -1,10 +1,5 @@
 package com.callcenter;
-
 import java.util.Date;
-
-/**
- * Created by Роман on 20.03.2016.
- */
 class Potential extends Call {
     public Potential(long tel, Date endTime) {
         super(tel, endTime);
