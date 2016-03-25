ff5691b19dbf883d06340e1a0b7bd010a0726794
scamscam <chubenkoroman@yandex.ru>
2016-03-25 16:47:15 +0300

    Update Tech.java

--- Tech.java
+++ Tech.java
@@ -1,10 +1,5 @@
 package com.callcenter;
-
 import java.util.Date;
-
-/**
- * Created by Роман on 20.03.2016.
- */
 class Tech extends Call {
     public Tech(long tel, Date endTime) {
         super(tel, endTime);
